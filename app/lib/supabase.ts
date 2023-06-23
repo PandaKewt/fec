0346f1ed831abd873f1d3edb977f6e8e4e11ccdd
pandakewt <chuminhtuan30306@gmail.com>
2023-06-23 20:17:25 +0700

    supabase api

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://rzhxfwfoyybqlyiflmmb.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || "";
+const supabaseKey = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aHhmd2ZveXlicWx5aWZsbW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcyMTk2MzMsImV4cCI6MjAwMjc5NTYzM30";
 export const supabase = createClient(supabaseUrl, supabaseKey);
